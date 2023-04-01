cfb87d55b44e89624a53527f20bfc3d5e2d1f544
LopesACF <altemilsonlopes@gmail.com>
2023-04-01 12:55:47 -0300

    Troco

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double valorGasto, valorPago, troco;
+Console.Write("Digite o valor que foi gasto: R$ ");
+
+valorGasto = double.Parse(Console.ReadLine()!);
+Console.Write("Digite o valor que foi pago: R$ ");
+valorPago = double.Parse(Console.ReadLine()!);
+troco = valorPago - valorGasto;
+Console.WriteLine("O valor do troco é: R$ " + troco.ToString());
+
